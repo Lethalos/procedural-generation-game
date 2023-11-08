df2631009af6cef23403a4f592e981ca30a6cef6
Kaan Ozdogan <necdetkaanoz@hotmail.com>
2023-11-08 10:38:39 +0300

    Add greater control over noise

--- Assets/_Scripts/MapGenerator.cs
+++ Assets/_Scripts/MapGenerator.cs
@@ -6,15 +6,29 @@ public class MapGenerator : MonoBehaviour
 {
     [SerializeField] int mapWidth;
     [SerializeField] int mapHeight;
+    [SerializeField] int seed;
     [SerializeField] float noiseScale;
+    [SerializeField] int octaves;
+    [Range(0f, 1f)]
+    [SerializeField] float persistance;
+    [SerializeField] float lacunarity;
+    [SerializeField] Vector2 offset;
 
     public bool autoUpdate;
 
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
         MapDisplay mapDisplay = gameObject.GetComponent<MapDisplay>();
         mapDisplay.DrawNoiseMap(noiseMap);
+    }
+
+    private void OnValidate()
+    {
+        if(mapWidth < 1) mapWidth = 1;
+        if(mapHeight < 1) mapHeight = 1;
+        if(lacunarity < 1) lacunarity = 1;
+        if(octaves < 0) octaves = 0;
     }
 }
